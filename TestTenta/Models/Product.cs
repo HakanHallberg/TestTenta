ffd75bc30ce14e9a7e9b7767d8eaeeed57e8630c
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2018-01-24 09:47:37 +0100

    Added model Products

---
+++ TestTenta/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTenta.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int UnitPrice { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}

fdce4d1e7b8e27259494091594d6ff3c81e2e8d7
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2018-01-24 09:54:08 +0100

    Set price range

--- TestTenta/Models/Product.cs
+++ TestTenta/Models/Product.cs
@@ -11,6 +11,7 @@ namespace TestTenta.Models
         public int ProductId { get; set; }
         [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
+        [Range(1, 1000000)]
         public int UnitPrice { get; set; }
         public DateTime ReleaseDate { get; set; }
         public bool IsDeleted { get; set; }

4656d3b0d8fd993f265e2541f2e5fd6b58b63646
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2018-01-24 09:52:08 +0100

    Name length minimum 3

--- TestTenta/Models/Product.cs
+++ TestTenta/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace TestTenta.Models
     public class Product
     {
         public int ProductId { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
         public int UnitPrice { get; set; }
         public DateTime ReleaseDate { get; set; }
